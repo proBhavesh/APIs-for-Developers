31e3c5dce7bd04e13d42f9d61a9a37505d720a61
Bhavesh <bhaveshjat9950@gmail.com>
2021-02-19 16:17:26 +0530

    reponsive sidebar added

--- app.js
+++ app.js
@@ -108,3 +108,28 @@ function move() {
 		}
 	}
 }
+
+//hide nav bar on scroll
+
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function () {
+// 	var currentScrollPos = window.pageYOffset;
+// 	if (prevScrollpos > currentScrollPos) {
+// 		document.getElementById("search-bar-id").style.top = "0";
+// 	} else {
+// 		document.getElementById("search-bar-id").style.top = "-50px";
+// 	}
+// 	prevScrollpos = currentScrollPos;
+// };
+
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+	document.getElementById("links-id").style.width = "250px";
+	// document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+	document.getElementById("links-id").style.width = "0";
+	// document.getElementById("main").style.marginLeft = "0";
+}
